fa92239c18eb054df18dd5d7de5d903a8d9eb31a
Reactdv <jerllylara12@gmail.com>
2022-06-02 04:50:09 +0800

    market

--- Screens/Bottom/HomeComponents/WetradeCup.js
+++ Screens/Bottom/HomeComponents/WetradeCup.js
@@ -3,7 +3,7 @@ import React from "react"
 import styled from "styled-components/native"
 import {useState,useLayoutEffect} from "react"
 import { StatusBar } from 'expo-status-bar';
-import { ScrollView,FlatList,SafeAreaView,TouchableOpacity,TextInput,StyleSheet, Text, View } from 'react-native';
+import {Image, ScrollView,FlatList,SafeAreaView,TouchableOpacity,TextInput,StyleSheet, Text, View } from 'react-native';
 
 
 
@@ -30,22 +30,30 @@ export default function Wetrade
   
   return (
     
-    <View style={container}>
-    
-    
-    
-  
-   <TouchableOpacity>
-   <Text>
-   press me
-   </Text>
-   </TouchableOpacity>
-    
-    
-    
-    
-    
+<View style={container}>
+  <View style={tw`flex flex-row items-center justify-between py-2.5 px-2.5 w-full`}> 
+   
+       <TouchableOpacity
+        onPress={()=>navigation.goBack()}>
+    <Feather name="chevron-left" size={24} color="black" /> </TouchableOpacity>
+    <Text style={tw`text-lg font-medium`}>Wetrade Cup </Text>
+    <View style={[tw`py-1.5 px-1.5 border-blue-600 rounded-2xl`,{borderWidth:1}]}>
+       <Text style={tw`text-blue-600 font-light text-sm`}>My prizes </Text>
     </View>
+  </View> 
+
+  <View style={tw`flex items-center Justify-center bg-indigo-600 w-full h-full  `}>
+    <Text style={tw`tracking-widest text-3xl text-amber-400 font-extrabold`}>Wetrade Cup</Text>
+    
+    <Text style={tw`text-xl text-white font-bold`}>2022 season 1</Text>
+  
+    <Text style={tw`text-white text-2xl font-bold`}>May 9 - July 13</Text>
+    <Image 
+     style={[{width:200,height:200},tw`bg-gradient-to-b from-amber-200 to yellow-100`]}
+     resizeMode="contain" 
+      source={require("../../../assets/images/championship-cup.png")}/>
+  </View>
+</View>
     
   );
 }
@@ -54,4 +62,4 @@ export default function Wetrade
 //{=================STYLES================}
 
 
-const container = tw`flex flex-auto items-center Justify-center bg-blue-400 `
+const container = tw`flex flex-auto items-center Justify-center  `
--- Screens/Bottom/Market.js
+++ Screens/Bottom/Market.js
@@ -3,7 +3,7 @@ import React from "react"
 import styled from "styled-components/native"
 import {useState,useLayoutEffect} from "react"
 import { StatusBar } from 'expo-status-bar';
-import { ScrollView,FlatList,SafeAreaView,TouchableOpacity,TextInput,StyleSheet, Text, View } from 'react-native';
+import {Image, ScrollView,FlatList,SafeAreaView,TouchableOpacity,TextInput,StyleSheet, Text, View } from 'react-native';
 
 import tw from "twrnc"
 
@@ -22,7 +22,74 @@ Ionicons.loadFont();
 
 
 
-
+const topMovers = [
+  
+  {
+    id:1,
+    name: "TRXUSD",
+    img: require("../../assets/images/tron.png"),
+    price: "0.09042",
+    percent:"10.50% ↑",
+  },
+  
+  {
+    id:2,
+    name: "NEOUSD",
+    img: require("../../assets/images/neousd.png"),
+    price: "11.48",
+    percent:"10.50% ↑",
+  },
+  
+  {
+    id:3,
+    name: "AAVE",
+    img: require("../../assets/images/AAVE.png"),
+    price: "112.28",
+    percent:"10.50% ↑",
+  },
+  
+  {
+    id:4,
+    name: "ADAUSD",
+    img: require("../../assets/images/ada.png"),
+    price: "0.5651",
+    percent:"10.50% ↑",
+  },
+  
+  {
+    id:5,
+    name: "MKRUSD",
+    img: require("../../assets/images/mkr.jpeg"),
+    price: "1235.37",
+    percent:"10.50% ↑",
+  },
+  
+  {
+    id:6,
+    name: "XLMUSD",
+    img: require("../../assets/images/xlm.png"),
+    price: "0.13907",
+    percent:"10.50% ↑",
+  },
+  
+  {
+    id:7,
+    name: "XNG",
+    img: require("../../assets/images/xng.png"),
+    price: "8.681",
+    percent:"10.50% ↑",
+  },
+  
+  {
+    id:8,
+    name: "SOLUSD",
+    img: require("../../assets/images/sol.png"),
+    price: "42.62",
+    percent:"10.50% ↑",
+  },
+  
+  
+  ]
 
 export default function Market
 ({navigation}) {
@@ -30,22 +97,35 @@ export default function Market
   
   return (
     
-    <View style={container}>
-    
-    
-    
-  
-   <TouchableOpacity>
-   <Text>
-  market
-   </Text>
-   </TouchableOpacity>
-    
-    
-    
-    
-    
-    </View>
+<View style={container}>
+  <View>
+     <Text style={tw`text-lg font-extrabold`}> Top mover </Text>
+     <ScrollView 
+     showsHorizontalScrollIndicator={false}
+     horizontal={true}style={[tw`flex flex-row flex-auto pt-2.5 h-auto`,{gap:10}]}>
+     
+        {
+          topMovers.map(({id,name,img,price,percent})=>{
+            return (
+              
+  <View key={id} style={[tw`rounded-md w-26 h-26 p-2.5 `,{marginRight:10,gap:5,borderWidth:1,borderColor:"#bac6c97c"}]}>
+            <View style={tw`flex flex-row items-center justify-start`}>
+           <Image style={{marginRight:5,width:20, height:20,borderRadius:10}}resizeMode="cover"source={img}/>
+           <Text style={tw`text-md font-light`}>{name}</Text> </View>
+           <Text>{price}</Text>
+           <Text style={tw`text-md font-semibold text-green-500`}>
+           {percent}</Text>
+        </View>
+              
+              )
+          })
+          
+        }
+       
+     </ScrollView>
+     
+  </View>  
+</View>
     
   );
 }
@@ -54,4 +134,4 @@ export default function Market
 //{=================STYLES================}
 
 
-const container = tw`flex flex-auto items-center Justify-center `
+const container = tw`flex  my-4 mx-2.5 `
